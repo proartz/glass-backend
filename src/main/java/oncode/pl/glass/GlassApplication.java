48f7069449eb40f7ee841bd3c80f9fa6294a442e
Marcin Filipowski <proartz.devon@gmail.com>
2019-07-05 13:37:09 +0200

    Create Spring Boot project

---
+++ src/main/java/oncode/pl/glass/GlassApplication.java
@@ -0,0 +1,13 @@
+package oncode.pl.glass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlassApplication.class, args);
+    }
+
+}
